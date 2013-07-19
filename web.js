aef3f65d3c81c88e8921a7bb22f5b69d41f263ac
Kevin Chen <kevin.chen.nj@gmail.com>
2013-07-19 21:52:17 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var newbuf = Buffer(30);
+newbuf = fs.readFileSync('index.html');
+var newstr = newbuf.toString();
+
 app.get('/', function(request, response) {
-  response.send("Hello World Test!");
+  response.send(newstr);
 });
 
 var port = process.env.PORT || 5000;
