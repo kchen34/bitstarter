53ca753023c25250f07576bcdcad9903048192c0
Kevin Chen <kevin.chen.nj@gmail.com>
2013-07-19 20:51:17 +0000

    A few changes to web.js

--- web.js
+++ web.js
@@ -2,11 +2,17 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var buf = fs.readFileSync('index.html');
+var str = buf.toString(buf); 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
